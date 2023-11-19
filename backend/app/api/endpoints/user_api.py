eda46620fab7fb5a42f8bbb19b85d97de2a19767
Kohei Komuro <82309237+k-sml@users.noreply.github.com>
2023-11-19 20:45:05 +0900

    create-user完成

--- backend/app/api/endpoints/user_api.py
+++ backend/app/api/endpoints/user_api.py
@@ -1,31 +1,31 @@
 from fastapi import APIRouter
-from ...crud.user_crud import select_all_user, create_user, delete_user, select_user, update_user
-from ...schemas.user_schema import UserCreate, User
+from crud.user_crud import select_all_user, create_user, delete_user, select_user, update_user
+from schemas.user_schema import UserCreate, UserResponseModel
 from typing import List
 
 router = APIRouter()
 
-@router.get("/users/", response_model=List[User])
+@router.get("/users/", response_model=List[UserResponseModel])
 def read_users():
     users = select_all_user()
     return users
 
-@router.get("/users/{user_id}", response_model=User)
+@router.get("/users/{user_id}", response_model=UserResponseModel)
 def read_user(user_id: str):
     user = select_user(user_id)
     return user
 
-@router.post("/users/", response_model=User)
+@router.post("/users/", response_model=UserCreate)
 def create_new_user(user: UserCreate):
     create_user(user.name, user.email, user.password)
     return user
 
 @router.patch("/users/{user_id}")
-def update_user_information(user_id: str):
-    update_user(user_id)
+def update_user_information(user_id: str, user: UserCreate):
+    update_user(user_id, user.name, user.email, user.password)
     return {"status": "user updated"}
 
-@router.delete("users/{user_id}")
+@router.delete("/users/{user_id}")
 def remove_user(user_id: str):
     delete_user(user_id)
     return {"status": "user deleted"}
--- backend/app/crud/user_crud.py
+++ backend/app/crud/user_crud.py
@@ -1,38 +1,39 @@
 # -*- encoding: utf-8 -*-
 import sys
-import models
-import databases
+from models.user import User
+from databases import create_new_session
 
 # .pycファイルの生成を防ぐ
 sys.dont_write_bytecode = True
 
 def select_all_user():
-    session = databases.create_new_session()
-    user_list = session.query(models.user).filter(models.user.status == 'created').all()
+    session = create_new_session()
+    user_list = session.query(User).filter(User.status == 'created').all()
     if user_list == None:
         user_list = []
     return user_list
 
 def create_user(user_name, user_email, user_password):
-    session = databases.create_new_session()
-    user = models.user()
+    session = create_new_session()
+    user = User()
     user.name = user_name
     user.email = user_email
     user.password = user_password
+    user.status = "created"
     session.add(user)
     session.commit()
     return 0
 
 def select_user(user_id):
-    session = databases.create_new_session()
-    user = session.query(models.user).filter(models.user.id == user_id).first()           
+    session = create_new_session()
+    user = session.query(User).filter(User.id == user_id).first()           
     if user == None:
         user = ""
     return user
 
 def update_user(user_id, user_name, user_mail, user_password):
-    session = databases.create_new_session()
-    user = session.query(models.user).filter(models.user.id == user_id).first()
+    session = create_new_session()
+    user = session.query(User).filter(User.id == user_id).first()
     if user == None:
         return 1
     user.name = user_name
@@ -42,8 +43,8 @@ def update_user(user_id, user_name, user_mail, user_password):
     return 0
 
 def delete_user(user_id):
-    session = databases.create_new_session()
-    user = session.query(models.user).filter(models.user.id == user_id).first()
+    session = create_new_session()
+    user = session.query(User).filter(User.id == user_id).first()
     if user == None:
         return 1
     user.status = "deleted"
